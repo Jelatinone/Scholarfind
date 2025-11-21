2a004806f803a3ece090bfcfe9fcc47d3df84824
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2025-11-21 09:44:44 -0600

    Make Distinctions Between `Task` Kinds

--- src/main/java/com/github/jelatinone/scholarfind/Main.java
+++ src/main/java/com/github/jelatinone/scholarfind/Main.java
@@ -1,5 +1,7 @@
 package com.github.jelatinone.scholarfind;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
@@ -18,54 +20,50 @@ import org.apache.commons.cli.ParseException;
 import com.github.jelatinone.scholarfind.meta.Task;
 
 import lombok.AccessLevel;
-import lombok.NonNull;
 import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public final class Main {
 	static final Logger _logger = Logger.getLogger(Main.class.getName());
-
 	static final CommandLineParser _parser = new DefaultParser();
+	static final Options _config = new Options();
 
 	static ExecutorService _executor;
 	static Map<Task<?, ?>, Future<?>> _tasks = new ConcurrentHashMap<>();
 
 	public static void main(String... arguments) {
-		Options configuredOptions = new Options();
-
 		Option opt_helpMessage = new Option("help", "print a descriptive help message");
-		configuredOptions.addOption(opt_helpMessage);
+		_config.addOption(opt_helpMessage);
 		Option opt_maxThreads = Option.builder("maxThreads")
 				.argName("with")
 				.desc("number of threads to run a given operation with")
 				.valueSeparator('=')
 				.get();
-		configuredOptions.addOption(opt_maxThreads);
-
+		_config.addOption(opt_maxThreads);
 		Option opt_executorType = Option.builder("executorType")
 				.argName("as")
 				.desc("Type of executor to run a given operation with")
-				.converter(ExecutorType::valueOf)
+				.converter((value) -> ExecutorType.valueOf(value.toUpperCase()))
 				.get();
-		configuredOptions.addOption(opt_executorType);
+		_config.addOption(opt_executorType);
 		Option opt_logToFile = Option.builder("logFile")
 				.argName("file")
 				.hasArg()
 				.valueSeparator(' ')
 				.desc("file to log resulting data to")
 				.get();
-		configuredOptions.addOption(opt_logToFile);
+		_config.addOption(opt_logToFile);
 
-		Option taskMarker = Option.builder()
+		Option opt_task = Option.builder()
 				.longOpt("task")
 				.hasArg()
-				.desc("Type of task <search, annotate, etc...>")
+				.desc("Defines a given task with arguments.")
 				.get();
-		configuredOptions.addOption(taskMarker);
+		_config.addOption(opt_task);
 
 		try {
 			_logger.fine(String.format("Supplied with command : %s", String.join(" ", arguments)));
-			CommandLine command = _parser.parse(configuredOptions, arguments);
+			CommandLine command = _parser.parse(_config, arguments);
 			_logger.fine(String.format("Parsed as command : %s", String.join(" ", command.getArgs())));
 			Integer maxThreads = command.<Integer>getParsedOptionValue(opt_maxThreads);
 			ExecutorType executorType = command.<ExecutorType>getParsedOptionValue(opt_executorType);
@@ -87,27 +85,33 @@ public final class Main {
 					break;
 			}
 
-			String[] kinds = command.getOptionValues(taskMarker);
-
-			if (kinds == null) {
+			String[] kindArguments = command.getOptionValues(opt_task);
+			if (kindArguments == null) {
 				_logger.severe("Could not parse argument(s) : no runnable tasks found!");
 				return;
 			}
 
-			for (String kind : kinds) {
-				final Task<?, ?> task = TaskType
-						.valueOf(kind).generator
-						.apply(command);
-				final Future<?> future = _executor.submit(task);
-				_tasks.put(task, future);
+			List<String> kindBlock = new ArrayList<>();
+			for (final String kindArgument : kindArguments) {
+				TaskKind maybeKind;
+				try {
+					maybeKind = TaskKind.valueOf(kindArgument);
+				} catch (final IllegalArgumentException exception) {
+					maybeKind = null;
+				}
+				if (maybeKind != null && !kindBlock.isEmpty()) {
+					submit(kindBlock);
+				}
+				kindBlock.add(kindArgument);
 			}
-
+			submit(kindBlock);
 		} catch (final ParseException exception) {
 			String message = String.format("Could not parse argument(s) : %s", exception.getMessage());
 			_logger.severe(message);
 			exception.printStackTrace();
 		}
 		try {
+			long initialTime = System.currentTimeMillis() / 1000;
 			_logger.fine(String.format("Executing [%s] tasks", _tasks.size()));
 			while (!_tasks.values().stream().allMatch(Future::isDone)) {
 				System.out.flush();
@@ -115,7 +119,7 @@ public final class Main {
 					String message = String.format(
 							"%s [%s] :: %s",
 							Task.getName(),
-							Task.getState(),
+							Math.floor(System.currentTimeMillis() / 1000 - initialTime),
 							Task.report());
 					_logger.fine(message);
 					System.out.println(message);
@@ -126,28 +130,38 @@ public final class Main {
 		}
 	}
 
-	static enum TaskType {
+	private static void submit(List<String> kindArguments) {
+		String[] kindBlock = kindArguments.toArray(String[]::new);
+		try {
+			CommandLine kindCommand = _parser.parse(_config, kindBlock);
+			TaskKind kind = TaskKind.valueOf(kindArguments.get(0).toUpperCase());
+
+			Task<?, ?> task = kind.generator.apply(kindCommand);
+			Future<?> future = _executor.submit(task);
+
+			_tasks.put(task, future);
+		} catch (final ParseException exception) {
+			String message = String.format("Could not parse argument(s) : %s", exception.getMessage());
+			_logger.severe(message);
+			System.out.println(message);
+			exception.printStackTrace();
+		}
+	}
+
+	static enum TaskKind {
 		;
 
-		final String value;
 		final Function<CommandLine, Task<?, ?>> generator;
 
-		private TaskType(final @NonNull String value, final Function<CommandLine, Task<?, ?>> generator) {
-			this.value = value;
+		private TaskKind(final Function<CommandLine, Task<?, ?>> generator) {
 			this.generator = generator;
 		}
 	}
 
 	static enum ExecutorType {
-		FIXED("fixed"),
-		WORK_STEALING("stealing"),
-		SCHEDULED("scheduled"),
-		VIRTUAL("virtual");
-
-		final String value;
-
-		private ExecutorType(final @NonNull String value) {
-			this.value = value;
-		}
+		FIXED,
+		WORK_STEALING,
+		SCHEDULED,
+		VIRTUAL;
 	}
 }
